d305f062527665b06693d5db3f43216f5e8d132b
WallasM <wallasmadeira@gmail.com>
2026-01-20 00:48:26 +0000

    vetor

---
+++ Vetor.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Vetor{
+    public static void main(String[] args){
+        Scanner sc = new Scanner (System.int);
+        Scanner sc = new Scanner (System.String)
+        System.out.println("Quantas pessoas tem na sala");
+        int Quant = sc.nextInt();
+        int Cont = 1;
+        while (Quant >= cont ){
+            System.out.println("Nome do aluno");
+            String Nome = sc.nextString;
+        }
+        
+    }
+}
