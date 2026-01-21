722073dc5c10f986bbef4f7fdb3cba487a922047
WallasM <wallasmadeira@gmail.com>
2026-01-21 00:30:52 +0000

    vetor2

--- Vetor.java
+++ Vetor.java
@@ -1,15 +1,31 @@
 import java.util.Scanner;
-public class Vetor{
-    public static void main(String[] args){
-        Scanner sc = new Scanner (System.int);
-        Scanner sc = new Scanner (System.String)
-        System.out.println("Quantas pessoas tem na sala");
-        int Quant = sc.nextInt();
-        int Cont = 1;
-        while (Quant >= cont ){
-            System.out.println("Nome do aluno");
-            String Nome = sc.nextString;
+
+public class Vetor {
+    public static void main(String[] args) {
+        // Perguntar quantidade de alunos
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Quantas pessoas tem na sala ");
+        int Quant = Integer.parseInt(scanner.nextLine());
+        // Nomes e idades de alunos
+        String nomes[] = new String[Quant];
+        int idade[] = new int[Quant];
+
+        for (int i = 0; i < Quant; i++) {
+            System.out.println("Nome do aluno: ");
+            nomes[i] = sc.nextLine();
+            sc.nextLine();
+
+            System.out.println("Idade do aluno: ");
+            idade[i] = sc.nextInt();
+            sc.nextLine();
         }
-        
-    }
+        for (int i = 0; i < Quant; i++) {
+            System.out.print(idade[i]);
+            System.out.printf(nomes[i]);
+        } 
+
+        sc.close();
+
+    }  
+
 }
