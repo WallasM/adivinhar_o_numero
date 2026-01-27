4b03a2af37f30c7a262b20b271808a1da10c3391
WallasM <wallasmadeira@gmail.com>
2026-01-27 00:53:33 +0000

    somar linha

---
+++ somarlinhas.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class somarlinhas {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[][] matriz = new int[4][4];
+        //Preencher a matriz
+        System.out.println("Digite os 16 numeros da matriz");
+        for (int i = 0; i < 4; i++){
+            for (int j = 0; j < 4; j++){
+                matriz[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println("Qual linha deseja somar?");
+        int linha = sc.nextInt();
+        //Aqui vai ficar o codigo pra somar a linha
+        
+
+    }
+}
