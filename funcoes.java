b9dbbe76de7d23be5282213d46f412802f8f9894
WallasM <wallasmadeira@gmail.com>
2026-01-28 00:46:52 +0000

    funçoes java

---
+++ funcoes.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class funcoes {
+    public static double calcularIMC(double peso, double altura) {
+        double imc = (peso / (altura * altura));
+        return imc;
+    }
+
+    public static double classificarIMC (double pesoNormal, double sobrePeso, double obesidade){
+        double
+
+
+    }
+    //public static void main(String[] args) {
+    //    Scanner sc = new Scanner(System.in);
+    //    System.out.println("Diga seu peso: ");
+    //    double peso = nextInt;
+    //    System.out.println("Diga sua altura: ");
+    //    double altura = next
+    //}
+
+}
